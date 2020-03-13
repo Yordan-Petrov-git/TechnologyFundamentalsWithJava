b456b68eeafa19a1a169107b8783abd28d8480e8
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-13 22:06:54 +0200

    HW exc 3 done

--- src/TextProcessingandRegularExpressions/Main2.java
+++ src/TextProcessingandRegularExpressions/Main2.java
@@ -3,6 +3,8 @@ package TextProcessingandRegularExpressions;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main2 {
@@ -15,21 +17,85 @@ public class Main2 {
 
     public static void main(String[] args) throws IOException {
 //==============================================================
-//1.	Repeat strings
+        //1.	Repeat strings
         // repeatStrings();
+
         //==============================================================
         // 2.	Substring
         //substring();
-        //==============================================================
 
         //==============================================================
+        //  3. reverseString();
+        //reverseString();
+
+        //==============================================================
+        //4.	Text Filter
+
+        //==============================================================
+        //5.	Digits, Letters and Other
 
         //==============================================================
 
+        //--------------------------------------------------------
+        //  Regular Expressions
+        //-----------------------------------------------------------
+
+        //  6.	Match Full Name
+
         //==============================================================
+        //7.	Match Phone Number
+
+        //==============================================================
+        //8.	Match Dates
+        // matchDates();
+        //==============================================================
+        // 9.	Match Numbers
+
+//==============================================================
 
 
+    }
 
+    private static void reverseString() throws IOException {
+        String input = "";
+        while (!"end".equals(input)) {
+
+            input = reader.readLine();
+
+            String reverse = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reverse += input.charAt(i);
+            }
+
+            if (!input.equals("end")) {
+                System.out.println(String.format("%s = %s", input, reverse));
+            }
+
+        }
+
+    }
+
+    private static void matchDates() throws IOException {
+        String regexInput = reader.readLine();
+
+
+        final String regex = "(?<day>[0-9]{2})(?<separator>\\/|\\.|\\-)(?<month>[A-z]{1}[a-z]{2})\\2(?<year>[0-9]{4})";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(regexInput);
+
+
+        while (matcher.find()) {
+
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+
+            System.out.println(
+                    String.format("Day: %s, Month: %s, Year: %s"
+                            , day
+                            , month
+                            , year));
+        }
     }
 
     private static void substring() throws IOException {
@@ -43,6 +109,7 @@ public class Main2 {
             lineSecond =
                     lineSecond.substring(0, index)
                             + lineSecond.substring(index + keyWord.length());
+
             index = lineSecond.indexOf(keyWord);
         }
         System.out.println(lineSecond);
@@ -53,6 +120,7 @@ public class Main2 {
         String[] word = (reader.readLine().split("\\s+"));
 
         StringBuilder sb = new StringBuilder();
+
         for (String s : word) {
             for (int i = 0; i < s.length(); i++) {
                 sb.append(s);
