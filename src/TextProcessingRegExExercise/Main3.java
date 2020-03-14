7aeecc91e0c2380b06c9ab804f9d21eda9e6cd78
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-14 03:09:42 +0200

    regex exc 1 done 100%

--- src/TextProcessingRegExExercise/Main3.java
+++ src/TextProcessingRegExExercise/Main3.java
@@ -1,7 +1,72 @@
-package TextProcessingRegExExercise;
+    package TextProcessingRegExExercise;
 
-public class Main3 {
-    public static void main(String[] args) {
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+
+    public class Main3 {
+        public static BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                System.in
+                        )
+                );
+
+        public static void main(String[] args) throws IOException {
+
+    //=======================================================
+           // 1.	Valid Usernames
+            validateUsers();
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+            //=======================================================
+
+
+        }
+
+        private static void validateUsers() throws IOException {
+            String[] input = reader.readLine().split(", ");
+
+            for (String s : input) {
+                if (validatorNameLength(s)&&validatorNameContent(s)){
+                    System.out.println(s);
+                }
+            }
+        }
+
+        public static boolean validatorNameLength(String nameCheck) {
+            int minRange = 3;
+            int maxRange = 16;
+            if (nameCheck.length() < minRange || nameCheck.length() > maxRange) {
+                return false;
+            }
+            return true;
+        }
+
+        public static boolean validatorNameContent(String nameCheck) {
+
+            String validate = "_-";
+
+            for (int i = 0; i < nameCheck.length(); i++) {
+
+                if (!Character.isLetterOrDigit(nameCheck.charAt(i))
+                        && nameCheck.charAt(i) != '-'
+                        && nameCheck.charAt(i) != '_') {
+
+                    return false;
+                }
+            }
+            return true;
+        }
 
     }
-}
